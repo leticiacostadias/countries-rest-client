18f2a57f2dfc0a2548f603aac327c178cbf6eef6
Leticia Dias <ledias@uolinc.com>
2020-04-25 14:19:19 -0300

    Changing fetch countries function to filter data results

--- src/scripts/DAOs/countriesDAO.js
+++ src/scripts/DAOs/countriesDAO.js
@@ -3,5 +3,7 @@ import Request from "../lib/request";
 const { COUNTRIES_API } = process.env;
 
 export async function getAll() {
-  return Request.get({ path: `${COUNTRIES_API}/all` });
+  return Request.get({
+    path: `${COUNTRIES_API}/all?fields=flag;name;population;region;capital;`,
+  });
 }
